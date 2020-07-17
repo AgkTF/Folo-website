2b05b9f38e5c329d23cbfb0a488e55b0967765a1
agkz1st <ahmed.agk22@gmail.com>
2020-07-17 18:54:31 +0200

    Add the burger button and the required JS code.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-const burgerBtn = document.querySelector(".hamburger--spin");
+const burgerBtn = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".navbar-menu--mobile");
+
+burgerBtn.addEventListener("click", () => {
+  burgerBtn.classList.toggle("is-active");
+
+  mobileMenu.classList.toggle("navbar-menu--hidden");
+});
